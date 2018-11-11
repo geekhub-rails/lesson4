f91d4053fd2d0af2c800bcf7c334c2891e2898e5
somenickname <just.leks@gmail.com>
2018-11-11 20:09:08 +0200

    add validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,16 @@
 class Task < ApplicationRecord
   validates :title, presence: true
+
+  validates :description,
+            presence: true,
+            length: {minimum: 5}
+
+  validate :expire_at_cannot_be_in_the_past
+
+  def expire_at_cannot_be_in_the_past
+    if expire_at.present? && expire_at < Date.today
+      errors.add(:expire_at, "can't be in the past")
+    end
+  end
+
 end
