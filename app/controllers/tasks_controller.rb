0a42dcacf10d089975f57456b85d8ca3c3eb4475
somenickname <just.leks@gmail.com>
2018-11-11 19:58:22 +0200

    add descr and expire fields to view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,6 +18,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title)
+    params.require(:task).permit(:title, :description, :expire_at)
   end
 end
